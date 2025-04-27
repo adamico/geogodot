d321a8fb8b58e0af3d989c2c8face38191c07b18
Andrea D'Amico <andrea.damico@gmail.com>
2025-04-27 12:02:33 +0200

    added HUD scene

---
+++ scripts/hud.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+
+@onready var player: Player = $"../Player"
+@onready var character_pos_value: Label = $ColorRect/GridContainer/CharacterPosValue
+@onready var sprite_pos_value: Label = $ColorRect/GridContainer/SpritePosValue
+
+func _process(delta: float) -> void:
+	update_character_position_value(player.get_node("Character").global_position)
+	update_sprite_position_value(player.get_node("Character/AnimatedSprite2D").global_position)
+
+func update_character_position_value(value) -> void:
+	character_pos_value.text = str(value)
+
+func update_sprite_position_value(value) -> void:
+	sprite_pos_value.text = str(value)
