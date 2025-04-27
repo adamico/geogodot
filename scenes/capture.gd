95c9dcd951f9317c7f50902c064437a4473ea4b7
Andrea D'Amico <andrea.damico@gmail.com>
2025-04-27 13:11:15 +0200

    capture sounds

--- scenes/capture.gd
+++ scenes/capture.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 @onready var progress_bar: ProgressBar = $ProgressBar
+@onready var finished_capturing_sound: AudioStreamPlayer2D = $FinishedCapturing
+@onready var capturing_sound: AudioStreamPlayer2D = $Capturing
+
 @export var state_chart: StateChart
 
 func _ready() -> void:
@@ -14,11 +17,16 @@ func _on_capturing_state_processing(delta: float) -> void:
 		state_chart.send_event("successful_capture")
 
 func _on_capturing_state_exited() -> void:
+	capturing_sound.stop()
 	reset_progress_bar()
 
 func _on_finish_capturing_state_exited() -> void:
+	finished_capturing_sound.play()
 	reset_progress_bar()
 
 func reset_progress_bar() -> void:
 	progress_bar.value = 0
 	progress_bar.visible = false
+
+func _on_capturing_state_entered() -> void:
+	capturing_sound.play()
