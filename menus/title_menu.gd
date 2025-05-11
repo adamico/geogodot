48a96210c8fc21219c909869621c186c051eae7b
Andrea D'Amico <andrea.damico@gmail.com>
2025-05-11 16:52:22 +0200

    added a title screen (#43)
    play, settings and quit buttons
    empty settings menu with apply and back buttons

--- menus/title_menu.gd
+++ menus/title_menu.gd
@@ -1,6 +1,25 @@
-extends Control
+extends Node2D
+
+@onready var play_button: Button = %PlayButton
+@onready var options_button: Button = %OptionsButton
+@onready var quit_button: Button = %QuitButton
+@onready var main_menu: CenterContainer = %MainMenu
+@onready var settings: PanelContainer = %Settings
+
+func _ready() -> void:
+    play_button.pressed.connect(_play)
+    options_button.pressed.connect(_show_options_menu)
+    quit_button.pressed.connect(_quit)
 
 
-func _process(_delta: float) -> void:
-    if Input.is_action_just_pressed("ui_accept"):
-        get_tree().change_scene_to_file("res://main.tscn")
+func _play() -> void:
+    get_tree().change_scene_to_file("res://main.tscn")
+
+
+func _show_options_menu() -> void:
+    main_menu.hide()
+    settings.show()
+
+
+func _quit() -> void:
+    get_tree().quit()
