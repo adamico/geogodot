acfba682c7a8476d5b839d5566b1664754acdb89
Andrea D'Amico <andrea.damico@gmail.com>
2025-05-13 19:26:33 +0200

    shoot target red flash

--- components/shoot_component.gd
+++ components/shoot_component.gd
@@ -17,9 +17,14 @@ const GUN = preload("res://assets/sprites/gun.png")
 func fire_laser() -> void:
     state_chart.send_event("shoot")
 
+func stop_firing() -> void:
+    create_tween().tween_property(target_component, "self_modulate", Color(1,1,1,0), 0.2)
+
 func _shoot() -> void:
-    assert(laser_scene is PackedScene,
-            "Error: the scene export was never set on this spawner component")
+    target_component.texture = GUN
+    target_animation_player.play("shoot_target_flash_red")
+    shoot_sound.play()
+
     _spawn_laser(target_component.global_position)
     var size_components = get_tree().get_nodes_in_group("size")
     for size_component in size_components:
@@ -27,6 +32,8 @@ func _shoot() -> void:
 
 
 func _spawn_laser(spawn_position) -> void:
+    assert(laser_scene is PackedScene,
+        "Error: the scene export was never set on this spawner component")
     var laser: Node2D = laser_scene.instantiate()
     laser.position = spawn_position
     laser.direction = target_component.direction
@@ -34,16 +41,7 @@ func _spawn_laser(spawn_position) -> void:
     actor.add_sibling(laser)
 
 
-func _on_cooldown_state_exited() -> void:
-    var tween = create_tween()
-    tween.tween_property(target_component, "self_modulate", Color(1,1,1,0), 0.2)
-
-
 func _on_cooldown_state_entered() -> void:
     var shoot_cooldown = 1.0 / (stats_component.laser_power + 1)
     to_can_shoot.delay_seconds = shoot_cooldown
-    target_component.texture = GUN
-    create_tween().tween_property(target_component, "self_modulate", Color(1,1,1,1), 0.2)
-
-    shoot_sound.play()
     _shoot()
