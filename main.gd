0118c07ac323eee769eedb70e1e35b9f084393c3
Andrea D'Amico <andrea.damico@gmail.com>
2025-05-17 16:34:57 +0200

    wave system

--- main.gd
+++ main.gd
@@ -1,20 +1,29 @@
 extends Node
 
+signal last_enemy_in_wave_dead
+
 @export var enemy_scene: PackedScene
 @export var mapping_context: GUIDEMappingContext
 
-var spawned_enemies: int = 0
-var max_enemies:= 10
 var game_start_time:= 0.0
 var current_time:= 0.0
 var elapsed_time:= 0.0
+var waves: Array[int]
+var current_wave:= 0
+var enemies_spawned:= 0
+var enemies_left: int
+var number_of_waves:= 5
+var time_to_next_wave:= 0.0
 
-@onready var start_timer: Timer = $StartTimer
+@onready var wave_timer: Timer = $WaveTimer
 @onready var enemy_timer: Timer = $EnemyTimer
 @onready var player: Player = $Player
 @onready var p_1_start_position: Marker2D = $P1StartPosition
 @onready var path_follow_2d: PathFollow2D = %PathFollow2D
 @onready var camera_2d: Camera2D = %Camera2D
+@onready var enemies_left_container: HBoxContainer = %EnemiesLeftContainer
+@onready var wave_number_label: Label = %WaveNumberLabel
+@onready var wave_number_value: Label = %WaveNumberValue
 
 
 func _ready() -> void:
@@ -25,31 +34,60 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
     current_time = Time.get_unix_time_from_system()
     elapsed_time = current_time - game_start_time
+    time_to_next_wave = wave_timer.time_left
+
 
 func _new_game() -> void:
+    _generate_waves()
     player.position = p_1_start_position.position
-    start_timer.start()
     game_start_time = Time.get_unix_time_from_system()
+    last_enemy_in_wave_dead.emit()
 
 
-func _on_player_dead() -> void:
+func _generate_waves() -> void:
+    var base_number_of_enemies:= 10
+    var number_of_enemies = base_number_of_enemies
+    waves.resize(number_of_waves)
+    for n in range(0, waves.size()):
+        number_of_enemies += base_number_of_enemies * n / 2
+        waves[n] = number_of_enemies
+
+
+func _game_over() -> void:
     enemy_timer.stop()
 
 
-func _on_start_timer_timeout() -> void:
+func _new_wave() -> void:
+    if current_wave > number_of_waves: return
+    enemies_spawned = 0
     enemy_timer.start()
+    enemies_left = waves[current_wave]
+
+
+func _on_player_dead() -> void:
+    _game_over()
 
 
 func _on_enemy_timer_timeout() -> void:
-    if spawned_enemies > max_enemies: return
+    if enemies_spawned >= waves[current_wave]: return
     var enemy: Node2D = enemy_scene.instantiate()
     path_follow_2d.progress_ratio = randf()
 
     enemy.position = path_follow_2d.position + camera_2d.get_screen_center_position()
     enemy.dead.connect(_on_enemy_death)
     add_child(enemy)
-    spawned_enemies += 1
+    enemies_spawned += 1
 
 
 func _on_enemy_death() -> void:
-    spawned_enemies -= 1
+    enemies_left -= 1
+    if enemies_left == 0: last_enemy_in_wave_dead.emit()
+
+
+func _on_wave_timer_timeout() -> void:
+    current_wave += 1
+    _new_wave()
+
+
+func _on_last_enemy_in_wave_dead() -> void:
+    wave_timer.start()
--- ui/hud.gd
+++ ui/hud.gd
@@ -1,5 +1,6 @@
 extends Control
 
+
 const RANK_1 = preload("res://assets/sprites/rank041.png")
 const RANK_2 = preload("res://assets/sprites/rank042.png")
 const RANK_3 = preload("res://assets/sprites/rank043.png")
@@ -8,14 +9,19 @@ const RANKS = [null, RANK_1, RANK_2, RANK_3, RANK_4]
 const POWER_BAR_FILL_STYLE = preload("res://ui/power_bar_fill_style.tres")
 const POWER_BAR_FILL_STYLE_MAX = preload("res://ui/power_bar_fill_style_max.tres")
 
-@export var game_timer: Timer
-
 var players: Array[Node]
-
 
 @onready var percentage_value: Label = %PercentageValue
 @onready var score_value: Label = %ScoreValue
 @onready var game_timer_value: Label = %GameTimerValue
+@onready var wave_number_container: CenterContainer = %WaveNumberContainer
+@onready var wave_number_value: Label = %WaveNumberValue
+@onready var wave_number_label: Label = %WaveNumberLabel
+@onready var next_wave_label: Label = %NextWaveLabel
+@onready var next_wave_value: Label = %NextWaveValue
+@onready var next_wave_container: CenterContainer = %NextWaveContainer
+@onready var enemies_left_value: Label = %EnemiesLeftValue
+@onready var enemies_left_container: HBoxContainer = %EnemiesLeftContainer
 @onready var root_node: Node = $".."
 
 
@@ -28,7 +34,12 @@ func _process(_delta: float) -> void:
     score_value.text = "%06d" % Score.score_values[1]
     ["capture_power", "laser_power"].map(_process_labels_for)
     var time_string = Time.get_time_string_from_unix_time(root_node.elapsed_time)
-    game_timer_value.text = time_string.substr(3)#"{minute}:{second}".format(time_dict)
+    game_timer_value.text = time_string.substr(3)
+    wave_number_value.text = str(root_node.current_wave)
+    enemies_left_value.text = str(root_node.enemies_left)
+
+    var time_to_next_wave = root_node.time_to_next_wave
+    if time_to_next_wave > 0: next_wave_value.text = "%.0f" % root_node.time_to_next_wave
 
 
 func _process_labels_for(stat_name) -> void:
@@ -47,3 +58,16 @@ func _process_labels_for(stat_name) -> void:
         else:
             progress_bar.value = Constants.POWER_SHARDS
             progress_bar.add_theme_stylebox_override("fill", POWER_BAR_FILL_STYLE_MAX)
+
+
+func _on_wave_timer_timeout() -> void:
+    wave_number_container.modulate = Color.WHITE
+    enemies_left_container.modulate = Color.WHITE
+    next_wave_container.hide()
+
+
+
+func _on_main_last_enemy_in_wave_dead() -> void:
+    wave_number_container.modulate = Color(1,1,1,0)
+    enemies_left_container.modulate = Color(1,1,1,0)
+    next_wave_container.show()
