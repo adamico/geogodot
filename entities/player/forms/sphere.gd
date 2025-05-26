a8a7c66e978c6b5c76b10de71291f79ef717e8e7
Andrea D'Amico <andrea.damico@gmail.com>
2025-05-26 16:31:33 +0200

    added script ide and animation refactor, triangle form 2

--- entities/player/forms/sphere.gd
+++ entities/player/forms/sphere.gd
@@ -1,9 +1,15 @@
 @tool
 extends Node2D
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+func _ready() -> void:
+    animation_player.play("pulse")
+
 
 func _draw() -> void:
-    draw_circle(Vector2.ZERO, 8, Color.BLACK, 1.0)
+    draw_circle(Vector2.ZERO, 6, Color.BLACK, 1.0)
 
 
 func _process(_delta: float) -> void:
--- entities/player/forms/triangle/triangle_piece.gd
+++ entities/player/forms/triangle/triangle_piece.gd
@@ -1,18 +1,18 @@
 @tool
 extends Node2D
 
-@export var vertex1:= Vector2(-0.866, -0.50)
-@export var vertex2:= Vector2(-0.433, 0.25)
-@export var vertex3:= Vector2.ZERO
-
+@export var vertex1: Vector2
+@export var vertex2: Vector2
+@export var vertex3: Vector2
+@export var offset:= Vector2(-0.433, 0.25)
+var scale_vector:= Vector2(20.0, 20.0)
 var tri: PackedVector2Array
 
 
 func _ready() -> void:
-    var scale_value = 20.0
-
     var tri_points: Array = [ vertex1, vertex2, vertex3, vertex1 ]
-    tri = scale_points(tri_points, scale_value)
+    tri = scale_points(tri_points)
+    position = offset * scale_vector
 
 
 func _process(delta: float) -> void:
@@ -20,10 +20,10 @@ func _process(delta: float) -> void:
 
 
 func _draw() -> void:
-    var godot_blue : Color = Color("478cbf")
-    draw_polygon(tri, [godot_blue])
+    draw_set_transform(-offset * scale_vector)
+    draw_polygon(tri, [Color.MEDIUM_BLUE])
     draw_polyline(tri, Color.ANTIQUE_WHITE)
 
 
-func scale_points(points: Array, scale_value) -> PackedVector2Array:
-    return points.map(func(point): return point * Vector2(scale_value, scale_value))
+func scale_points(points: Array) -> PackedVector2Array:
+    return points.map(func(point): return point * scale_vector)
